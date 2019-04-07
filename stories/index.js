ed31ceacb45216d14d6472c2c0fff552ff5489a4
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-07 13:18:14 +0200

    fix: linter

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { storiesOf } from '@storybook/react'
 import centered from '@storybook/addon-centered'
 import { Copy } from 'react-feather'
