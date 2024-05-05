e7a62e01b27a141662e55bfd2bf6a6e8e9562f5d
Brad Adams <hi@breadadams.com>
2024-05-05 18:20:31 +0200

    build: update storybook to v8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,18 @@
-module.exports = {
-  stories: ['../stories/**/*.js'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials']
+const config = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/addon-webpack5-compiler-babel'
+  ],
+
+  docs: {
+    autodocs: 'tag'
+  },
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  }
 }
+export default config
